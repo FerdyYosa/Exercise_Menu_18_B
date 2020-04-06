6dd4503c424c513c16f22f2ce5f2f2fd0dd7919e
Muhammad Ferdy Yosa <ferdyzecter@gmail.com>
2020-04-06 14:30:14 +0700

    First Commit, configure background and text color for the login page

---
+++ app/src/main/java/com/example/exercisemenu18b/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.exercisemenu18b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button submit;
+    EditText Nama, Pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        submit = findViewById(R.id.btnok);
+        Nama = findViewById(R.id.editUN);
+        Pass = findViewById(R.id.editPass);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                if (Pass.getText().toString().equals("123") && Nama.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Password Salah", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}

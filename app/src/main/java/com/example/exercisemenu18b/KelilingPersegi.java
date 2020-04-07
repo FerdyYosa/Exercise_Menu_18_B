1db7033d56df938b4c7476fb42db10ed8cf50e78
Muhammad Ferdy Yosa <ferdyzecter@gmail.com>
2020-04-07 22:37:04 +0700

    Adding function in page keliling for square shape

--- app/src/main/java/com/example/exercisemenu18b/KelilingPersegi.java
+++ app/src/main/java/com/example/exercisemenu18b/KelilingPersegi.java
@@ -17,5 +17,19 @@ public class KelilingPersegi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keliling_persegi);
+        edP = findViewById(R.id.edP);
+        hKP = findViewById(R.id.hKP);
+        bttnKP = findViewById(R.id.bttnKP);
+        bttnKP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s1;
+                s1 = edP.getText().toString();
+                double n1, n2;
+                n1 = Double.parseDouble(s1);
+                n2 = n1 * 4;
+                hKP.setText("Hasil keliling persegi dengan sisi: " + n1 + " adalah: " + n2);
+            }
+        });
     }
 }

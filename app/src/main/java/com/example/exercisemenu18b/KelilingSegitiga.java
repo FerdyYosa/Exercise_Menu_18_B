d5a7cddae98c7db4bcef0d937f466b869e0293b7
Muhammad Ferdy Yosa <ferdyzecter@gmail.com>
2020-04-07 23:02:15 +0700

    Adding function in page keliling for triangle shape

--- app/src/main/java/com/example/exercisemenu18b/KelilingSegitiga.java
+++ app/src/main/java/com/example/exercisemenu18b/KelilingSegitiga.java
@@ -3,12 +3,39 @@ package com.example.exercisemenu18b;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class KelilingSegitiga extends AppCompatActivity {
+    Button bttnKP;
+    EditText edP, edP2, edP3;
+    TextView hKP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keliling_segitiga);
+        edP = findViewById(R.id.edP);
+        edP2 = findViewById(R.id.edP2);
+        edP3 = findViewById(R.id.edP3);
+        hKP = findViewById(R.id.hKP);
+        bttnKP = findViewById(R.id.bttnKP);
+        bttnKP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s1, s2, s3;
+                s1 = edP.getText().toString();
+                s2 = edP2.getText().toString();
+                s3 = edP3.getText().toString();
+                double n1, n2, n3, n4;
+                n1 = Double.parseDouble(s1);
+                n2 = Double.parseDouble(s2);
+                n3 = Double.parseDouble(s3);
+                n4 = n1 + n2 + n3;
+                hKP.setText("Keliling Segitiga dengan alas: " + n1 + " , tinggi: " + n2 + "dan sisi miring:" + n3 + " adalah: " + n4);
+            }
+        });
     }
 }

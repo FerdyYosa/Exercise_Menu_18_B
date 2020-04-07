0fe67f85f58b03557a235f84c6e51faba949303c
Muhammad Ferdy Yosa <ferdyzecter@gmail.com>
2020-04-07 23:22:59 +0700

    Adding function in page keliling for remaining shape

--- app/src/main/java/com/example/exercisemenu18b/KelilingLingkaran.java
+++ app/src/main/java/com/example/exercisemenu18b/KelilingLingkaran.java
@@ -3,12 +3,33 @@ package com.example.exercisemenu18b;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class KelilingLingkaran extends AppCompatActivity {
 
+    Button bttnKP;
+    EditText edP;
+    TextView hKP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keliling_lingkaran);
+        edP = findViewById(R.id.edP);
+        hKP = findViewById(R.id.hKP);
+        bttnKP = findViewById(R.id.bttnKP);
+        bttnKP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s1;
+                s1 = edP.getText().toString();
+                double n1, n2;
+                n1 = Double.parseDouble(s1);
+                n2 = 2*3.14*n1;
+                hKP.setText("Hasil Keliling Lingkaran dengan jari-jari: " + n1 + " adalah: " + n2);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/exercisemenu18b/KelilingPersegiPanjang.java
+++ app/src/main/java/com/example/exercisemenu18b/KelilingPersegiPanjang.java
@@ -3,12 +3,36 @@ package com.example.exercisemenu18b;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class KelilingPersegiPanjang extends AppCompatActivity {
 
+    Button bttnKP;
+    EditText edP, edP2;
+    TextView hKP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keliling_persegi_panjang);
+        edP = findViewById(R.id.edP);
+        edP2 = findViewById(R.id.edP2);
+        hKP = findViewById(R.id.hKP);
+        bttnKP = findViewById(R.id.bttnKP);
+        bttnKP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s1, s2, s3;
+                s1 = edP.getText().toString();
+                s2 = edP2.getText().toString();
+                double n1, n2, n3, n4;
+                n1 = Double.parseDouble(s1);
+                n2 = Double.parseDouble(s2);
+                n3 = n1 + n2 + n1 + n2;
+                hKP.setText("Keliling Persegi Panjang dengan panjang: " + n1 + " dan lebar " + n2 + " adalah: " + n3);
+            }
+        });
     }
 }
